3222833c6197b2f5e1de680274d50186067a506c
Rodolfo Oliveira <rodolfo@gmail.com>
2025-06-22 10:36:58 -0300

    adjusts margin

--- src/shared/components/FullScreenLoader.tsx
+++ src/shared/components/FullScreenLoader.tsx
@@ -2,8 +2,11 @@ import { ImSpinner2 } from 'react-icons/im'
 
 export default function FullScreenLoader() {
   return (
-    <div data-testid="full-screen-loader" className="fixed inset-0 bg-black/60 z-50 flex items-center justify-center">
-      <ImSpinner2 size={48} className="text-white animate-spin" />
+    <div
+      data-testid="full-screen-loader"
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 m-0 p-0"
+    >
+      <ImSpinner2 size={48} className="text-white animate-spin m-0 p-0" />
     </div>
   )
 }
